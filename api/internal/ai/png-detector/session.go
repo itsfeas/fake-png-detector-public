be2bbbb350614a5af0b38ba1a25e0bb532b34f10
itsfeas <amf.fiaz@gmail.com>
2024-06-13 21:55:36 -0600

    remove singleton in png detector session

--- api/internal/ai/png-detector/session.go
+++ api/internal/ai/png-detector/session.go
@@ -1,32 +1,26 @@
-package ai
+package png_detector
 
 import (
 	ort "github.com/yalue/onnxruntime_go"
 )
+
+const ImageSize = 224
 
 type FakePngDetectorModel struct {
 	*ort.Session[float32]
 	ImageSize int16
 }
 
-const ImageSize = 224
-
-var fakePngDetector FakePngDetectorModel
-
-func GetFakePngDetectorModel() *FakePngDetectorModel {
-	return &fakePngDetector
-}
-
-func (model *FakePngDetectorModel) InitializeSession(path string) error {
+func InitializeSession(modelPath string) (*FakePngDetectorModel, error) {
 	err := ort.InitializeEnvironment()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	inputShape := ort.NewShape(1, 3, ImageSize, ImageSize)
 	inputTensor, err := ort.NewEmptyTensor[float32](inputShape)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer inputTensor.Destroy()
 
@@ -34,14 +28,14 @@ func (model *FakePngDetectorModel) InitializeSession(path string) error {
 	outputTensor, err := ort.NewEmptyTensor[float32](outputShape)
 	defer outputTensor.Destroy()
 
-	session, err := ort.NewSession[float32](path,
+	session, err := ort.NewSession[float32](modelPath,
 		[]string{"modelInput"}, []string{"modelOutput"},
 		[]*ort.Tensor[float32]{inputTensor}, []*ort.Tensor[float32]{outputTensor})
 	defer session.Destroy()
 
-	fakePngDetector = FakePngDetectorModel{
+	sessionWrapper := FakePngDetectorModel{
 		session,
 		ImageSize,
 	}
-	return nil
+	return &sessionWrapper, nil
 }
