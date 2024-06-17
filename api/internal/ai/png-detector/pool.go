51e7c1aa98c9b6ab21278fcda6ab79074e974858
itsfeas <amf.fiaz@gmail.com>
2024-06-16 19:52:30 -0600

    update method for pool

--- api/internal/ai/png-detector/pool.go
+++ api/internal/ai/png-detector/pool.go
@@ -7,5 +7,48 @@ type SessionPool struct {
 	queue       []int8
 	modelPath   string
 	mut         *sync.Mutex
+	cond        *sync.Cond
 	maxSessions int8
+}
+
+func InitializePool(maxSessions int8, modelPath string) error {
+	newPool := SessionPool{
+		sessions:    make([]*FakePngDetectorSession, maxSessions),
+		queue:       make([]int8, maxSessions),
+		modelPath:   modelPath,
+		mut:         new(sync.Mutex),
+		cond:        new(sync.Cond),
+		maxSessions: maxSessions,
+	}
+	newPool.mut.Lock()
+	for i := range maxSessions {
+		newSession, err := InitializeSession(modelPath)
+		if err != nil {
+			return err
+		}
+		newPool.sessions[i] = newSession
+		newPool.queue = append(newPool.queue, i)
+	}
+	newPool.mut.Unlock()
+	return nil
+}
+
+func GetSession(pool *SessionPool) *FakePngDetectorSession {
+	pool.mut.Lock()
+	for len(pool.queue) == 0 {
+		pool.cond.Wait()
+	}
+	sessionId := pool.queue[0]
+	session := pool.sessions[]
+	pool.queue = pool.queue[1:]
+	pool.mut.Unlock()
+	defer FinishSession(pool, sessionId)
+	return session
+}
+
+func FinishSession(pool *SessionPool, idx int8) {
+	pool.mut.Lock()
+	pool.queue = append(pool.queue, idx)
+	pool.cond.Broadcast()
+	pool.mut.Unlock()
 }
