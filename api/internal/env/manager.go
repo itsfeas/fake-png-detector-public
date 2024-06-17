a31c592abc0a99d54ae9fa242e6fc4e3f34d4ede
itsfeas <amf.fiaz@gmail.com>
2024-06-16 19:55:45 -0600

    global environment variable cache

---
+++ api/internal/env/manager.go
@@ -0,0 +1,33 @@
+package env
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+var envMap map[string]string
+
+func GetEnvMap() *map[string]string {
+	return &envMap
+}
+
+func InitializeEnvMap(downloadDir string) error {
+	env, err := godotenv.Read()
+	if err != nil {
+		return err
+	}
+	envMap = env
+	return nil
+}
+
+func LoadEnvFile(path string) error {
+	home, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	if err := godotenv.Load(home + path); err != nil {
+		return fmt.Errorf("error loading .env file")
+	}
+	return nil
+}
