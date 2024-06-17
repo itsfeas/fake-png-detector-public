51ba7730a09160fb737f31c63b4dff2ebc9d962c
itsfeas <amf.fiaz@gmail.com>
2024-06-17 11:34:55 -0600

    add an initialization sequence for testing

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,1 +1,38 @@
-package cmd
+package main
+
+import (
+	pngdetector "fake-png-detector.mod/internal/ai/png-detector"
+	"fake-png-detector.mod/internal/env"
+	"fmt"
+)
+
+func main() {
+	if err := env.LoadEnvFile("./env/.env"); err != nil {
+		fmt.Printf("Switching to using OS env variables. Err: %v\n", err)
+	}
+
+	if err := env.InitializeEnvMap("./env/.env"); err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+
+	if err := pngdetector.InitializeSessionPool(); err != nil {
+		fmt.Printf("%v", err)
+		return
+	}
+
+	//http.HandleFunc("/huh", func(w http.ResponseWriter, r *http.Request) {
+	//	fmt.Fprintf(w, "<h1>Hi there, I'm resume-gunk-api!</h1>")
+	//})
+	//
+	//fmt.Println("Server is listening on port 8080...")
+	//if err := http.ListenAndServe(":8080", api.Routes()); err != nil {
+	//	fmt.Printf("Error starting the server: %v\n", err)
+	//}
+	fmt.Println("Hello, World!")
+	pool := pngdetector.GetSessionPool()
+	{
+		pngdetector.GetSession(pool)
+	}
+	fmt.Println("Hello, World!2")
+}
