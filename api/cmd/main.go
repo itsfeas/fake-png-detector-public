cc019566a8e4eb297f1762f7064fd5088be534f4
itsfeas <amf.fiaz@gmail.com>
2024-06-21 00:22:22 -0600

    load lib in initializatio

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -4,35 +4,31 @@ import (
 	pngdetector "fake-png-detector.mod/internal/ai/png-detector"
 	"fake-png-detector.mod/internal/env"
 	"fmt"
+	ort "github.com/yalue/onnxruntime_go"
 )
 
 func main() {
-	if err := env.LoadEnvFile("./env/.env"); err != nil {
-		fmt.Printf("Switching to using OS env variables. Err: %v\n", err)
-	}
+	//if err := env.LoadEnvFile("./env/.env"); err != nil {
+	//	fmt.Printf("Switching to using OS env variables. Err: %v\n", err)
+	//}
 
 	if err := env.InitializeEnvMap("./env/.env"); err != nil {
 		fmt.Printf("%v\n", err)
 		return
 	}
 
+	envMap := *env.GetEnvMap()
+	ort.SetSharedLibraryPath(envMap["ORT_LIB_PATH"])
+
 	if err := pngdetector.InitializeSessionPool(); err != nil {
 		fmt.Printf("%v", err)
 		return
 	}
 
-	//http.HandleFunc("/huh", func(w http.ResponseWriter, r *http.Request) {
-	//	fmt.Fprintf(w, "<h1>Hi there, I'm resume-gunk-api!</h1>")
-	//})
-	//
-	//fmt.Println("Server is listening on port 8080...")
-	//if err := http.ListenAndServe(":8080", api.Routes()); err != nil {
-	//	fmt.Printf("Error starting the server: %v\n", err)
-	//}
 	fmt.Println("Hello, World!")
 	pool := pngdetector.GetSessionPool()
 	{
-		pngdetector.GetSession(pool)
+		p := pngdetector.GetSession(pool)
 	}
 	fmt.Println("Hello, World!2")
 }
